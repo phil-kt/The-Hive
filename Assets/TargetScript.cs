9d075040ad723f240ed4456366a772955d125955
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-11-16 01:07:13 -0500

    setting up lighting for tree

---
+++ Assets/TargetScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.transform.tag == "Player"){
+ 			Application.LoadLevel(Application.loadedLevel);
+		}
+		print("hit");
+	 }
+}
